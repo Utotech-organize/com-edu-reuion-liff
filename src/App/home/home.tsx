fad6a159d095a8f1c940e367cf7be864ae8513e5
phuwis <phuwis.tweed@gmail.com>
2023-03-26 19:35:55 +0700

    fix: 🐛 (homepage)
    add image and set layout

--- src/App/home/home.tsx
+++ src/App/home/home.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Image } from "antd";
+
+import Giraffe from "../../Static/images/Giraffe.png";
+
 import "../../Static/styles/Layout.css";
 
 export default function Homepage() {
   return (
     <div className="">
-      <h1>Home Page</h1>
+      <div
+        style={{
+          width: "100%",
+          height: "50px",
+          backgroundColor: "#1E1E1E",
+          color: "white",
+        }}
+      >
+        App Bar
+      </div>
+      <h1 style={{ color: "#ffffff" }}>Welcome ComEdu Reunion</h1>
+      <Image width={100} src={Giraffe}></Image>
     </div>
   );
 }
